47b5a597aee5ad03b8153f11e55fde23d47a4c5c
lhycms <1404293933@qq.com>
2022-11-09 11:27:42 +0800

    reframe msubmenu3: rewrite opts

--- menu/mmenu/msubmenu/msubmenu3/menu.sh
+++ menu/mmenu/msubmenu/msubmenu3/menu.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-# 加载每 msubmenu3 对应的所有 opt 函数
-source $PWKIT_ROOT/menu/mmenu/msubmenu/msubmenu3/opt1.sh
-source $PWKIT_ROOT/menu/mmenu/msubmenu/msubmenu3/opt2.sh
+# 加载每 msubmenu3 对应的所有 opt 函数 -- Python click: opt1.py, opt2.py, ...
 
 
 mmenu::msubmenu3() {
@@ -18,9 +16,13 @@ while [ 1 ]
     bb|BB)
       mmenu ;;
     1)
-      msubmenu3::opt1 ;;
+      $PWKIT_ROOT/menu/mmenu/msubmenu/msubmenu3/opt1.py
+      exit 0
+      ;;
     2)
-      msubmenu3::opt2 ;;
+      $PWKIT_ROOT/menu/mmenu/msubmenu/msubmenu3/opt2.py
+      exit 0
+      ;;
     *)
       echo -e "\033[35m (*_*) Unsupported selection! Try Again... (*_*)\033[0m" ;;
     esac
---
+++ menu/mmenu/msubmenu/msubmenu3/opt2.py
@@ -0,0 +1,24 @@
+#!/data/home/liuhanyu/anaconda3/envs/workdir/bin/python3
+import click
+
+
+@click.command()
+def opt1():
+    ### Part I. 模块名
+    print("{0:=^80}".format(" Module --> PLUMED "))
+    print(
+'''1.模块简介
+----------
+  PLUMED是一个功能强大的软件，包括：增强的采样算法；自由能法；用于分析分子动力学（MD）
+模拟产生的大量数据的工具(使用元动力学计算表面自由能）。
+'''
+    )
+    print(
+'''2.使用手册
+----------
+  http://www.pwmat.com:3389/pwmat-resource/module-download7/pdf/Plumed_intro.pdf
+'''
+    )
+
+if __name__ == "__main__":
+    opt1()
