4c8ba43d93d65e9744f2a3b320f66a1a58165c34
lhycms <1404293933@qq.com>
2023-01-11 16:30:41 +0800

    Utilitys: Add function convert_rho.x

--- menu/umenu/usubmenu/usubmenu1/opt1/opt1_use.py
+++ menu/umenu/usubmenu/usubmenu1/opt1/opt1_use.py
@@ -9,9 +9,31 @@ def opt1():
 XcrySDen 可读的格式。
         2. 使用方法：convert_rho.x OUT.RHO，之后就会得到 RHO.xsf
     
-    '''
+    '''    
     os.system("convert_rho.x OUT.RHO")
 
+
+    current_path = os.getcwd()
+    rho_xsf_path = os.path.join(current_path, "RHO.xsf")
+    if os.path.exists(rho_xsf_path):
+        print_sum()
+
+
+def print_sum():
+    '''
+    Description
+    -----------
+        1. 输出 summary
+    '''
+    print("*{0:-^68}*".format(" Summary "))
+    
+    print("\t* 输入文件:")
+    print("\t\tOUT.RHO")
+    print("\t* 输出文件:")
+    print("\t\tRHO.xsf")
+    
+    
+    print("*{0:-^68}*".format("---------"))
 
 if __name__ == "__main__":
     opt1()
