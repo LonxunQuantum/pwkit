ca1cf318c5be03b78855ae1fc6e612844c1bb952
lhycms <1404293933@qq.com>
2023-01-12 11:35:49 +0800

    Utility: Add function absorption_spec_K.x

---
+++ menu/umenu/usubmenu/usubmenu2/opt2/opt2_use.py
@@ -0,0 +1,14 @@
+import os 
+
+
+def opt2():
+    '''
+    Description
+    -----------
+        1. plot_DOS.py 主要用于输出 DOS 图。
+    '''
+    os.system("plot_DOS.py")
+    
+    
+if __name__ == "__main__":
+    opt2()
