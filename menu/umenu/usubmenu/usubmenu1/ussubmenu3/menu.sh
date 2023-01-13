5b7c2ad77a5f18fc2f3accd661125e30dd77c8fe
lhycms <1404293933@qq.com>
2023-01-13 12:00:48 +0800

    查看晶胞的基矢

--- menu/umenu/usubmenu/usubmenu1/ussubmenu3/menu.sh
+++ menu/umenu/usubmenu/usubmenu1/ussubmenu3/menu.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 
-### 加载 usubmenu 对应的所有opt函数
-#for i in {1,2,3,4}
-#    do
-#        source $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu2/opt${i}/menu_opt${i}_use.sh
-#    done
+info_types_lst=([0]="1" [1]="2" [2]="3" [3]="4" [4]="5") 
+
 
 
 usubmenu1::ussubmenu3() {
@@ -15,6 +12,28 @@ while [ 1 ]
         read -p " ------------>>  
 " submenuOpt
 
+
+    if echo "${info_types_lst[@]}" | grep -w $submenuOpt &>/dev/null;
+    then
+        ### Step 1. 得到结构文件的`文件名` -- $atom_config_file_name
+        echo "
+ - 当前目录下共有 $(ls | wc -l) 个文件。搜索当前目录是否含有名为 atom.config 的结构文件..."
+
+        if [ ! -f "atom.config" ]; then
+            echo -e "
+\033[31m - 未搜索到名为atom.config的文件，需要手动指定结构文件名...\033[0m"
+            read -p " 
+ 结构文件的名字
+ --------------->>
+" atom_config_file_name
+        else
+            atom_config_file_name="atom.config"
+        fi
+    fi
+
+
+
+    ### Step 2. 根据输入，执行相关的行为
     case $submenuOpt in 
     q|Q)
         # 退出 pwkit 的 conda 环境
@@ -22,22 +41,31 @@ while [ 1 ]
         exit 0
         ;;
     bb|BB)
-        #umenu::usubmenu1   
+        umenu::usubmenu1
         ;;
     1)
-        #ussubmenu2::opt1   # 查看原晶胞的基矢
+        # 查看原晶胞的基矢
+        $PYTHON_PATH $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt1/opt1_use.py $atom_config_file_name
         exit 0
         ;;
     2)
-        #ussubmenu2::opt2   # 查看倒易点阵的基矢
+        # 查看原晶胞的基矢长度
+        $PYTHON_PATH $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt2/opt2_use.py $atom_config_file_name
         exit 0
         ;;
     3)
-        #ussubmenu2::opt3   # 查看原晶胞的基矢长度
+        # 查看原晶胞的基矢间夹角
+        $PYTHON_PATH $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt3/opt3_use.py $atom_config_file_name
         exit 0
         ;;
     4)
-        #ussubmenu2::opt4   # 查看原晶胞的基矢间夹角
+        # 查看倒易点阵的基矢 (单位: 2pi/埃)
+        $PYTHON_PATH $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt4/opt4_use.py $atom_config_file_name
+        exit 0
+        ;;
+    5)
+        # 查看倒易点阵的基矢 (单位: 1/埃)
+        $PYTHON_PATH $PWKIT_ROOT/menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt5/opt5_use.py $atom_config_file_name
         exit 0
         ;;
     *)
