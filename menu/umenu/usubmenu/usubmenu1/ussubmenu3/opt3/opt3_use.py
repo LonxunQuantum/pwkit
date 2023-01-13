5359f497a9715c8c208341dcb024eb8fe533ec06
lhycms <1404293933@qq.com>
2023-01-13 12:51:44 +0800

    获取基矢间的夹角

--- menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt3/opt3_use.py
+++ menu/umenu/usubmenu/usubmenu1/ussubmenu3/opt3/opt3_use.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import numpy as np
 from pflow.io.publicLayer.structure import DStructure
 
 
@@ -7,7 +8,7 @@ def get_basis_vector(atom_config_name:str):
     '''
     Description
     -----------
-        1. 查看晶胞的基矢长度
+        1. 查看晶胞的基矢间夹角
 
     Parameters
     ----------
@@ -22,7 +23,8 @@ def get_basis_vector(atom_config_name:str):
                             file_format="pwmat",
                             file_path=atom_config_path,
                             )
-    basis_vector_length_tuple = structure.lattice.abc
+    basis_vector_array = structure.lattice.matrix
+    print(basis_vector_array)
     
     ### Step 2. 输出信息
     # 1. 
@@ -32,18 +34,30 @@ def get_basis_vector(atom_config_name:str):
     print("\t\t{0}".format(atom_config_name))
     
     # 2. 
-    print("\t* 晶胞的基矢长度(单位:埃) :")
-    print("\t\t{0:<15}{1:<15}{2:<15}".format("a1-length", "a2-length", "a3-length"))
+    def gat_angle(array_1:np.array, array_2:np.array):
+        cos_angle = np.dot(array_1, array_2) / ( np.linalg.norm(array_1) * np.linalg.norm(array_2) )
+        angle_rad = np.arccos(cos_angle)
+        angle_deg = np.degrees(angle_rad)
+        return angle_deg
+    print("\t* 晶胞的基矢间夹角:")
+    
+    print("\t\t{0:<15}{1:<15}{2:<15}".format("alpha", "beta", "gamma"))
+    alpha_angle = gat_angle(array_1=basis_vector_array[0], array_2=basis_vector_array[1])
+    beta_angle = gat_angle(array_1=basis_vector_array[1], array_2=basis_vector_array[2])
+    gamma_angle = gat_angle(array_1=basis_vector_array[2], array_2=basis_vector_array[0])
+    
     print("\t\t{0:<15}{1:<15}{2:<15}".format(
-                                        round(basis_vector_length_tuple[0], 8),
-                                        round(basis_vector_length_tuple[1], 8),
-                                        round(basis_vector_length_tuple[2], 8)
+                                round(alpha_angle, 2),
+                                round(beta_angle, 2),
+                                round(gamma_angle, 2)
                                         )
         )
     
     
     # 3. 
     print("*{0:-^72}*".format("---------"))
+
+
 
 
 if __name__ == "__main__":
