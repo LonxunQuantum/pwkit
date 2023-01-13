02ef74c35e876bcc7ccc77dd1de6249881a017d7
lhycms <1404293933@qq.com>
2023-01-14 00:06:05 +0800

    结构转换: inp,pdb -> atom.config

--- menu/umenu/usubmenu/usubmenu1/ussubmenu1/menu_cn.py
+++ menu/umenu/usubmenu/usubmenu1/ussubmenu1/menu_cn.py
@@ -6,13 +6,16 @@ def mmenu_cn():
     #print("{0:=^80}".format(" Module "))
 
     ### Part I. 格式转换
-    print("{0:-^80}".format("  其他格式 -> PWMat格式  "))
+    print("{0:-^50}".format("  其他格式 -> PWMat格式  "))
     print(
-''' 
- 1) POSCAR文件 (POSCAR)    -> atom.config     2) xsf文件 (atom.xsf)   -> atom.config
- 3) atat文件 (rndstr.in)   -> atom.config     4) cif文件 (atom.cif)   -> atom.config     
- 5) pwscf文件 (atom.pwscf) -> atom.config     6) cell文件 (atom.cell) -> atom.config
- 7) pdb文件 (atom.pdb)     -> atom.config     8) inp文件 (atom.inp)   -> atom.config
+''' 1) POSCAR文件 (POSCAR)     -> atom.config
+ 2) xsf文件    (atom.xsf)   -> atom.config
+ 3) atat文件   (rndstr.in)  -> atom.config     
+ 4) cif文件    (atom.cif)   -> atom.config     
+ 5) pwscf文件  (atom.pwscf) -> atom.config     
+ 6) cell文件   (atom.cell)  -> atom.config
+ 7) pdb文件    (atom.pdb)   -> atom.config     
+ 8) inp文件    (atom.inp)   -> atom.config
 '''
     )
     
