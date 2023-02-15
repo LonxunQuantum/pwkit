7a9e8ff403ca516a982b5dca62bdbfa66b9caf7d
lhycms <1404293933@qq.com>
2023-02-15 20:02:15 +0800

    Fix the bug: KPOINTS coordinations -- in IN.KPT and REPORT

--- test/test.py
+++ test/test.py
@@ -1,15 +1,27 @@
+from pflow.io.publicLayer.structure import DStructure
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Define the k-points along the x-axis
-kpoints = np.linspace(-np.pi, np.pi, 100)
 
-# Calculate the eigenenergies at each k-point
-eigenenergies = np.cos(kpoints)
+### Part I.
+structure = DStructure.from_file(
+    file_format="pwmat", 
+    file_path="/data/home/liuhanyu/hyliu/pwmat_demo/band/atom.config",
+)
+basis = np.array(structure.lattice.reciprocal_lattice.matrix)
+print("\n倒格矢 (unit: 1/(2*pi)) : ")
+print(basis)
 
-# Plot the band structure
-plt.plot(kpoints, eigenenergies)
-plt.xlabel('k-point')
-plt.ylabel('Eigenenergy')
-plt.title('Band Structure')
-plt.savefig("/data/home/liuhanyu/hyliu/code/pwkit/test/demo.jpg")
+print("\n倒格矢 : ")
+print(np.array(structure.lattice.reciprocal_lattice_crystallographic.matrix))
+
+
+### Part II.
+a1 = np.array([
+      [0.000000,      0.000000,      0.000000],
+      [0.055556,      0.000000,      0.000000],           
+      [0.111111,      0.000000,      0.000000],         
+      [0.166667,      0.000000,      0.000000],        
+      [0.222222,      0.000000,      0.000000],
+            ])
+print("\n计算结果 : ")
+print( np.dot(a1, basis) * 0.529177249 )
