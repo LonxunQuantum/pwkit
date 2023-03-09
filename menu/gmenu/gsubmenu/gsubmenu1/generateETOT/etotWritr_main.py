5abf398929b0ccbe3b0afdf8b466af8ba923198f
lhycms <1404293933@qq.com>
2023-03-09 10:17:00 +0800

    EtotWriterMain -- write_functiion() && write_pseudo()

--- menu/gmenu/gsubmenu/gsubmenu1/generateETOT/etotWritr_main.py
+++ menu/gmenu/gsubmenu/gsubmenu1/generateETOT/etotWritr_main.py
@@ -9,9 +9,6 @@ import os
 import sys
 import subprocess
 from etotWriter import EtotWriter
-from select_task import *
-from select_functional import *
-from select_pseudo import *
 from select_specific import *
 
 
@@ -25,12 +22,11 @@ tasks_str_lst = [
             "TD", "TC", "TS"
 ]
 
-
 def get_upper_task_mark(task_str:str):
     '''
     Descritpion
     -----------
-        1. 得到 `task_mark` 的大写形式 (`sc` -> `SC`)
+        1. 得到 `task_mark/str` 的大写形式 (`sc` -> `SC`)
     
     Paramaters
     ----------
@@ -42,9 +38,60 @@ def get_upper_task_mark(task_str:str):
 
 
 '''
-Part I. Task Type
+Part II. Functional Type
 '''
+functionals_str_lst = [
+        'PE', '91', 'PS',
+        'LD', 'H6', 'H3',
+        'P0', 'B3', 'TP',
+        'SC',
+]
 
+def get_upper_functional_mark(functional_str:str):
+    '''
+    Description
+    -----------
+        1. 得到 `function_mark/str` 的大写形式
+    
+    Parameters
+    ----------
+        1. functional_mark: str
+            - 泛函的类型
+    '''
+    if functional_str == '':
+        return "default"
+    elif (functional_str.upper() not in functionals_str_lst):
+        return "default"
+    else:
+        return functional_str.upper()
+        
+
+'''
+Part III. Pseudo Potential Type
+'''
+pseudos_str_lst = [
+            'SG', 'PD', 
+            'FH', 'PW',
+            'UD',
+]
+
+def get_upper_pseudo_mark(pseudo_str:str):
+    '''
+    Description
+    -----------
+        1. 得到 `pseudo_mark/str` 的大写形式
+    
+    Parameters
+    ----------
+        1. pseudo_mark: str
+            - 赝势的类型
+    '''
+    if pseudo_str == '':
+        return "default"
+    elif (pseudo_str.upper() not in pseudos_str_lst):
+        return "default"
+    else:
+        return pseudo_str.upper()
 
 
 
@@ -73,9 +120,16 @@ class EtotWriterMain(object):
     
     
     def execute(self):
+        '''
+        Description
+        ----------- 
+            1. 
+        '''
         self.write_task()
-        #self.write_functional()
-        #self.write_accuracy_and_scf()
+        self.write_functional()
+        self.write_accuracy_and_scf()
+        self.write_pesudo()
+    
     
     
     def write_task(self):
@@ -83,6 +137,11 @@ class EtotWriterMain(object):
         Description
         -----------
             1. 设置`EtotWriter object`的`任务类型`属性
+        
+        Note
+        ----
+            1. 在 menu.sh 中，已经提前处理过 `task_mark -> task_name`，因此此处的 `task_mark` 不可能报错
+                - 仅调用 `get_upper_task_mark(task_str)` 即可
         '''
         ### Step 1. 得到前两个字符，作为 `task_str`
         p1 = subprocess.Popen(
@@ -119,7 +178,7 @@ class EtotWriterMain(object):
                         shell=False,
         )
         output, _ = p2.communicate()
-        self.long_str = output.decode()
+        self.long_str = output.decode().strip()
         
         ### Step 4. 终端输出信息，提醒用户设置完毕
         pp = subprocess.Popen(
@@ -131,6 +190,7 @@ class EtotWriterMain(object):
         output, _ = pp.communicate()
         
         print(output.decode().strip())
+   
     
     
     def write_functional(self):
@@ -153,13 +213,13 @@ class EtotWriterMain(object):
                     shell=False,
         )
         output, _ = p2.communicate()
-        functional_str = output.decode()
+        functional_str = output.decode().strip()    # 此处的`泛函类型`可能不是真的
         
         ### Step 2. `etot_writer` 赋予 `泛函类型`
-        functional_str = select_functional(functional_str=functional_str)
+        functional_str = get_upper_functional_mark(functional_str=functional_str)
         if functional_str == "default":
             self.etot_writer.write_functional(functional_name="PE")
-        if functional_str in functionals_str_lst:
+        else:
             self.etot_writer.write_functional(functional_name=functional_str)
         
             ### Step 3. 删去前两个代表 functional 的字符
@@ -176,8 +236,16 @@ class EtotWriterMain(object):
                         shell=True,
             )
             output, _ = p2.communicate()
-            self.long_str = output.decode()
-        print("Part II. 泛函类型设置成功..")
+            self.long_str = output.decode().strip()
+
+        ### Step 4. 在 terminal 输出提示信息，提醒用户输出成功
+        pp = subprocess.Popen(
+                    ['echo', "Part II. 泛函类型设置成功..."],
+                    stdout=subprocess.PIPE,
+                    shell=False
+        )
+        output, _ = pp.communicate()
+        print(output.decode().strip())
             
         
         
@@ -210,11 +278,39 @@ class EtotWriterMain(object):
                     shell=True,
         )
         output, _ = p2.communicate()
+        pseudo_str = output.decode()  # 此处的`赝势类型`可能不是真的 e.g. `scpesp`
         
-        ### Step 2. 设置赝势类型，后面使用
-        self.pseudo_str = output.decode()
+        ### Step 2. `etot_writer` 赋予 `赝势类型`
+        pseudo_str = get_upper_pseudo_mark(pseudo_str=pseudo_str)
+        if pseudo_str == "default":
+            self.pseudo_str_upper = "SG"
+        else:
+            self.pseudo_str_upper = pseudo_str
+
+            ### Step 3. 删去前两个代表 pseudo potential 的字符
+            p1 = subprocess.Popen(
+                        ['echo', '{0}'.format(self.long_str)],
+                        stdout=subprocess.PIPE,
+                        shell=False,
+            )
+            p1.wait()
+            p2 = subprocess.Popen(
+                        ['cut', '-c', '3-'],
+                        stdout=subprocess.PIPE,
+                        shell=False,
+            )
+            output, _ = p2.communicate()
+            self.long_str = output.decode().strip()
         
-        print("Part III. 赝势类型设置成功..")
+        ### Step 4. 
+        pp = subprocess.Popen(
+                    ['echo', 'Part III. 赝势类型设置成功...'],
+                    stdout=subprocess.PIPE,
+                    shell=False,
+        )
+        output, _ = pp.communicate()
+        print(output.decode().strip())
+    
     
     
     def write_specific(self):
