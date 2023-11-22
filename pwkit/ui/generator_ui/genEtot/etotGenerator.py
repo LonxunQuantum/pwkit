6b27b7b88b3d8dcb987b5eaea8500ee39d4025cf
lhycms <1404293933@qq.com>
2023-11-22 17:06:49 +0800

    Improve UITemplate, OPTTemplate

---
+++ pwkit/ui/generator_ui/genEtot/etotGenerator.py
@@ -0,0 +1,17 @@
+import os 
+import sys
+import subprocess
+from typing import Union, Tuple
+
+from ...ui_template import UITemplate, OPTTemplate
+from ....variable import *
+
+
+
+class EtotGenerator(OPTTemplate):
+    def show(self):
+        pass
+    
+    
+    def process_input(self):
+        pass
--- pwkit/ui/ui_template.py
+++ pwkit/ui/ui_template.py
@@ -1,15 +1,55 @@
 from abc import abstractmethod
-
+import os
+import sys
+from typing import Union
 
 class UITemplate(object):
     @abstractmethod
     def show(self):
-        pass
+        ### Part I.
+        print("{0:-^60}".format(" xxxx "))
+        print(
+''' 1) xxx                      2) xxx
+ 3) xxx                     4) xxx
+'''
+        )        
+        
+        ### Part II. Exit
+        print(
+'''
+ bb) 返回上一级目录
+ q)  退出'''
+        )
     
     
     @abstractmethod
     def process_input(self):
-        pass
+        back_mark:Union[None, str] = None
+
+        while (True):
+            if (back_mark == "BB"):
+                self.show()
+            user_choice:str = input("------------>> \n").upper()
+            
+            # Case 1. Exit the program
+            if (user_choice == "Q"):
+                try:
+                    sys.exit()
+                except SystemExit as e:
+                    break
+            
+            elif (user_choice == "1"):
+                # submenu.show()
+                # back_mark = submenu.process_input()
+                # if (back_mark == None)
+                #    break
+                # elif (back_mark == "BB")
+                #    continue
+                pass
+            
+            else:
+                print("(*_*) Unsupported input! Try again... (*_*)")
+            
     
     
     
